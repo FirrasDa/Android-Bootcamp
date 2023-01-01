29356e1be1f8483f51c0277d914a7cd721bebf5e
FirrasDa <108306473+FirrasDa@users.noreply.github.com>
2023-01-01 18:54:16 +0300

    Add files via upload

---
+++ Ex1-Variables.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val s: String = "Abdulallah"
+    val f: Float= 20.6F
+    val d:Double= 8.9746325
+    val sh:Byte=25
+    val i:Short=2022
+    val i2:Long=321984526954
+    val b:Boolean =true
+    val c:Char= 'c'
+
+}
