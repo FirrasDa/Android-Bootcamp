1ffd1479ca3b0b80aed7ce627d5d130aa4f1c127
FirrasDa <108306473+FirrasDa@users.noreply.github.com>
2023-01-02 21:25:08 +0300

    Add files via upload

---
+++ Ex2-Operators.kt
@@ -0,0 +1,21 @@
+package com.example.tuwiq
+
+fun main() {
+//    false && false = false
+//    10 % 3 = 1
+//    5 >= 5 = true
+//    5 - 3 * 2 = -1
+//    false || true = true
+//    3 != 2 = true
+//    (6 + 6) / 3 = 4
+    println( false && false)
+    println( 10 % 3)
+    println(5 >= 5)
+    println( 5 - 3 * 2)
+    println( false || true)
+    println( 3 != 2 )
+    println( (6 + 6) / 3)
+
+
+
+}
